fbd80f24468cef8ca03d46688d25e3f83c22024f
Jarquevious <jarquevious@gmail.com>
2020-10-19 15:20:03 -0700

    google api implemented

--- application/urls.py
+++ application/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('', views.index, name='home'),
+    path('vote/', views.vote, name='vote'),
+    path('check-vote-registration/', views.check_vote, name='check-vote'),
+    path('request-absentee-ballot/', views.absentee_ballot, name='absentee-ballot'),
+    path('election-reminder/', views.election_reminder, name='election-reminder'),
+    path('pledge-to-register/', views.pledge_register, name='pledge-register'),
+
 ]
--- application/views.py
+++ application/views.py
@@ -3,24 +3,114 @@ from django.http import HttpResponse
 from .form import QuestionForm
 from django.http import HttpResponse
 from .models import Question
+import requests, json
 
 
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    form = QuestionForm()
-    context = {'latest_question_list': latest_question_list, 'form': form}
+    # representative()
+    electioninfo = get_electionInfo()
+    locations = electioninfo["pollingLocations"]
+
+    print(json.dumps(locations, indent=4))
+    
+    context = {"locations": locations,
+                "electionInfo": electioninfo    
+            }
+    # get_elections()
+    # find_election()
     return render(request, 'application/index.html', context)
 
+def vote(request):
+    return render(request, 'application/vote-register.html')
+
+def check_vote(request):
+    return render(request, 'application/vote-check.html')
+
+def absentee_ballot(request):
+    return render(request, 'application/absentee-ballot.html')
+
+def election_reminder(request):
+    return render(request, 'application/election-reminder.html')
+
+def pledge_register(request):
+    return render(request, 'application/pledge-register.html')
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'application/detail.html', {'question': question})
+def find_election(query="president"):
+    url = "https://www.googleapis.com/civicinfo/v2/divisions"
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    querystring = {
+                    "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+                    "query": query,
+                }
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    headers = {
+        "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+    return response.json
+
+def get_representatives_by_address():
+    url = "https://www.googleapis.com/civicinfo/v2/representatives"
+
+    querystring = {
+                    "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+                    "address": "94108",
+                    "levels": "country",
+                    "roles": "legislatorUpperBody",
+                    "roles": "legislatorLowerBody",
+                }
+
+    headers = {
+        "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+    return response.json
+
+def get_elections():
+    url = "https://www.googleapis.com/civicinfo/v2/elections"
+
+    querystring = {
+                    "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+                }
+
+    headers = {
+        "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    print(response.text)
+
+    return response.json
+
+def get_electionInfo():
+    url = "https://www.googleapis.com/civicinfo/v2/voterinfo"
+
+    querystring = {
+                    "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+                    "address": "851 california st. san francisco, ca 94108",
+
+                }
+
+    headers = {
+        "key":"AIzaSyDID0q9BHOlG-8m_xgMd3-1tcqZiHXwZWs",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # print(response.text)
+
+    return response.json()
+
+
+
